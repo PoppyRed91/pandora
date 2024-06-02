a15d9776622c64893295224e49839b4c323ecb4f
marianordqvist <marianordqvist@live.se>
2024-06-02 11:29:58 +0200

    add scene 1 & 2

--- Scene2/scene2.js
+++ Scene2/scene2.js
@@ -1,36 +1,76 @@
-// let tl7 = gsap.timeline({scrollTrigger: {trigger: "#cloud-2", scrub: 1}})
-// .to("#cloud-2", {
-//   opacity: 1,
-//   y: 50,
-//   ease: "power3.out",
-// }).to("#cloud-2", {
-//   opacity: 0,
-// })
+let tl1 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".scene-2-background",
+      start: "top bottom",
+      end: "bottom top",
+      scrub: 1,
+    },
+  })
+  .to(".scene-2-background", {
+    //   x: -40,
+    scale: 1.3,
+    opacity: 1,
+  });
 
-// let tl8 = gsap.timeline({scrollTrigger: {trigger: "#cloud-2", start: "top bottom+=100px", end:"top top", scrub: 1}})
-//   .to("#cloud-2", {
-//   opacity: 0.8,
-//   ease: "power3.out",
-// }).to("#character-2", {
-//   opacity: 0.1,
-//   height: "35%",
-//   width: "35%",
-//   top:"40%",
-//   left:"20%",
-// }).to("#character-2", {
-//   height: "60%",
-//   width: "60%",
-//   top:"20%",
-//   left:"15%",
-//   opacity:0.8,
-// }).to("#character-2", {
-//   opacity: 0,
-//   ease: "power3.out",
-// })
+let tl2 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".scene-2-background",
+      start: "center bottom",
+      end: "bottom top",
+      scrub: 1,
+    },
+  })
+  .to(".scene-2-black-box", {
+    opacity: 0.7,
+  });
 
-// let tl9 = gsap.timeline({scrollTrigger: {trigger: "#text-2", scrub: 1}})
-//   .to("#text-2", {
-//     opacity:1
-//   }).to("#text-2", {
-//     opacity:0
-//   })
+let tl3 = gsap
+  .timeline({ scrollTrigger: { trigger: ".scene-2-background", scrub: 1 } })
+  .to(".scene-2-ground", {
+    opacity: 1,
+  })
+  .to(".scene-2-ground", {
+    opacity: 0,
+    ease: Power2.easeInOut,
+  });
+
+let tl4 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".scene-2-background",
+      start: "top bottom",
+      end: "bottom top",
+      scrub: 1,
+    },
+  })
+  .to(".scene-2-character", {
+    opacity: 1,
+  })
+  .to(".scene-2-character", {
+    delay: 0.3,
+    opacity: 0.2,
+    ease: Power3.easeOut,
+    x: "30",
+  });
+
+let tl5 = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".scene-2-background",
+      start: "top+=300 bottom",
+      end: "bottom top",
+      scrub: 1,
+    },
+  })
+  .to(".scene-2-text", {
+    opacity: 1,
+    y: "45",
+    stagger: 1,
+  })
+  .to(".scene-2-text", {
+    opacity: 0,
+    y: "-45",
+    stagger: 0.5,
+  });
